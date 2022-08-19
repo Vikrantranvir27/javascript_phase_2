280b150b176807f10a543a7624e8273daab107c3
Vikrantranvir27 <104892344+Vikrantranvir27@users.noreply.github.com>
2022-08-19 16:20:41 +0530

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,2 @@
+let name=prompt("how can i help you?");
+document.write( Problem);
---
+++ constant.js
@@ -0,0 +1,11 @@
+
+
+  try {
+    const PI = 3.14;
+    PI = 3.14;
+    document.write(PI);
+
+  }
+  catch (err) {
+    document.write(err);
+  }
